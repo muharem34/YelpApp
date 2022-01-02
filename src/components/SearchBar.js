ccb52af8b6b1f3057fd12801d905cdefb60e0399
Muharem Sumic <muharem22@gmail.com>
2022-01-01 21:27:31 -0600

    updated searchbar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,12 +3,12 @@ import React  from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { FontAwesome5 } from '@expo/vector-icons'; //importing the search icon from the expowebsite https://github.com/expo/vector-icons
 
-
 const SearchBar= () => {
     return (
+        
         <View style={styles.backgroundStyle}>
             {/*This is the css code from expowebsite to style the search icon */}
-            <FontAwesome5 name="search" size={30} color="black"/> 
+            <FontAwesome5 name="search" style={styles.iconStyle}/> 
             {/* here we are styling the text space    */}
             <TextInput style = {styles.inputStyle} placeholder="Search"/> 
         </View>
@@ -17,17 +17,30 @@ const SearchBar= () => {
 };
 
 const styles = StyleSheet.create({
+    //    We are aligning the parent here
     backgroundStyle: {
+        marginTop: 15,
         backgroundColor: 'lightgray',
-        height: 50,
-        borderRadius: 4,
-        marginHorizontal: 15,
+        height: 45,
+        borderRadius: 5,
+        marginHorizontal: 17,
         flexDirection: "row", // Shows the text input on same line as the search icon
     },
+        //   Aligning the Children
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        flex: 1   //Text will take as much space as needed when typing
+        // borderColor: 'black',
+        flex: 1,   //Text will take as much space as needed when typing but will be inside the text space
+        // borderWidth: 1,
+        flex: 1,
+        fontSize: 20
+    },
+        // Styling the Search Icon
+    iconStyle: {
+        fontSize: 34,
+        alignSelf: "center",
+        marginHorizontal: 15
+       
+
     }
 
 });
