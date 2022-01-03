21794cce7e2e59829c9a4c60e3093b5cc0336d4f
Muharem Sumic <muharem22@gmail.com>
2022-01-02 21:23:40 -0600

    deleted yelp.jsapis

--- src/api/yelp.js
+++
@@ -1,8 +0,0 @@
-import axios from axios;
-
-export default axios.create({
-    baseURL:'https://api.yelp.com/v3/businesses',
-    headers: {
-        Authorization: 'Bearer XIdWG61e3aU-6McyXVzpxEFXqWukDto_TFlS0W2OPQ2oyo-zCNE5Q_8E74vcxgfy5CUvt7g6d029hFb9BN2D2UbA_tK3zHyX2tq-axO4_uco0P8TE_Iuasb8X8zPYXYx'
-    }
-});
