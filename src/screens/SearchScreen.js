2d117b588480f07d5181dcd315934f8dad618497
Muharem Sumic <muharem22@gmail.com>
2022-01-02 23:14:29 -0600

    added Api network request and added errors message incase something goes wrong

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,20 +8,42 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
     const [term, setTerm] = useState(''); //cause we are using search so much we can name it term
     const [restaurants, setRestaurants] = useState([]); //useState something that updates, so this is the part of API 
+    const [errorMessage, setErrorMessage] = useState('');
+
+        // We are doing API network request here
+    const searchApi = async (searchTerm) => {
+        console.log("hi there");
+        try {
+            const response = await yelp.get('/search', {
+                params: { //paramaters
+                    limit: 50,
+                    term: searchTerm, //if any param are idential like here term=term, it is enough just to put term,
+                    location: 'saint louis'
+                }
+            });
+            setRestaurants(response.data.businesses); //we just want the businesses data results
+        } catch (err) {
+            setErrorMessage('Something went wrong!');
+        }
+    };
+
+    // Call search API when component
+    // is first rendered.  BAD CODE:
+    // searchApi('pasta');
+
     return (
         <View>
             {/* We want the Search Bar to appear above the Search Screen text so place it above search screen */}
             <SearchBar
                 term = {term} 
-                onTermChange={(newTerm) => setTerm(newTerm)}
-                onTermSubmit= {() => console.log('term was submitted')}
+                onTermChange={setTerm}
+                onTermSubmit={() => searchApi(term)}
              />
-            <Text>Search Screen</Text>
-            <Text>{term}</Text>
-
+             {/* if there is error then print the error message otherwise nothing */}
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
+            <Text>We have found {restaurants.length} restaurants</Text>
         </View>
     );
-
 };
 
 const styles = StyleSheet.create({
