ca308d2c84684d06dbb69e84246e93a55d26feaf
Muharem Sumic <muharem22@gmail.com>
2022-01-03 21:33:34 -0600

    Added hooks updated searchScreen.js by using api hook created useRestaurant.js hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,36 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 // since searchbar will be in the search screen page we need to import that SearchBar component in here
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
+import useRestaurants from "../hooks/useRestaurants";
 
 
 const SearchScreen = () => {
     const [term, setTerm] = useState(''); //cause we are using search so much we can name it term
-    const [restaurants, setRestaurants] = useState([]); //useState something that updates, so this is the part of API 
-    const [errorMessage, setErrorMessage] = useState('');
-
-        // We are doing API network request here
-    const searchApi = async (searchTerm) => {
-        console.log("hi there");
-        try {
-            const response = await yelp.get('/search', {
-                params: { //paramaters
-                    limit: 50,
-                    term: searchTerm, //if any param are idential like here term=term, it is enough just to put term,
-                    location: 'saint louis'
-                }
-            });
-            setRestaurants(response.data.businesses); //we just want the businesses data results
-        } catch (err) {
-            setErrorMessage('Something went wrong!');
-        }
-    };
-
-    // Call search API when component
-    // is first rendered.  BAD CODE:
-    // searchApi('pasta');
-
+    const [searchApi, restaurants, errorMessage ] = useRestaurants();
+  
     return (
         <View>
             {/* We want the Search Bar to appear above the Search Screen text so place it above search screen */}
